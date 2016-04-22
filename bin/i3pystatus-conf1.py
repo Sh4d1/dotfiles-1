096482f3f0aefb7fd5309559cb933ff0cae2bcbf
Samuel Walladge <samuel@swalladge.id.au>
2016-04-22 20:02:06 +0930

    ok, so now lets try i3pystatus

---
+++ bin/i3pystatus-conf1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from i3pystatus import Status
+
+status = Status()
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+                format="%Y-%m-%d %H:%M:%S")
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+status.register("cpu_usage",
+                format="CPU {usage:3.0f}%")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+                alert_temp=70,
+                format="{temp:.0f}°C",)
+
+
+# Shows the address and up/down state of eth0.
+# If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+status.register("network",
+                interface="eno1",
+                format_up="{v4cidr}",)
+
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+                path="/",
+                format="Home {avail}G",)
+
+status.register("disk",
+                path="/mnt/storage/",
+                format="Storage {avail}G",)
+
+
+# Shows pulseaudio default sink volume
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+                format="♪ {volume}%",)
+
+status.register("cmus",
+                format="{status} {title} - {artist}",
+                color="#7070ff")
+
+
+status.run()
