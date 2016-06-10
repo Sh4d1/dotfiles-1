effa94b63bd6af08f881e00bd9e8ed7fe2a8a9da
Samuel Walladge <samuel@swalladge.id.au>
2016-06-10 20:56:05 +0930

    add tmux options + script to kill session

---
+++ bin/kill-and-switch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# kills the current tmux session and let's you choose another session
+# won't kill if is only session running
+
+SESSION=`tmux list-panes -F '#{session_name}'`
+if tmux switch-client -p && tmux kill-session -t $SESSION; then
+  if [ `tmux list-sessions | wc -l` -gt 1 ]; then
+    tmux choose-session;
+  fi
+fi
+
+# exit cleanly to avoid output in tmux
+true
+
